f5ea76a0754cfb4cf2f54daca126e2518a5e5e7c
Eduart2021 <zeedymarina@yahoo.com>
2025-10-29 14:10:59 -0500

    adding delete function

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ const nameInput = document.getElementById('task-name');
 const categoryInput = document.getElementById('task-category');
 const deadlineInput = document.getElementById('task-deadline');
 const addTaskBtn = document.getElementById('add-task');
+const deleteBtn = document.getElementById('delete-last');
 
 // DB
 let tasks = [];
 let idCount = 0
 
-// Add new 
+// Add new task
 function addTask(){
     const inName = nameInput.value
     const inCategory = categoryInput.value
     const deadlineEntry = deadlineInput.value
     const inDeadLine = deadlineInput.value
-
+    idCount= tasks.length+1
+    
     let newTask = {
-        id: idCount++,
+        id: idCount,
         name:inName,
         category: inCategory,
         deadline: inDeadLine,
@@ -23,9 +25,17 @@ function addTask(){
     }
    tasks.push(newTask)
 }
+// Delete a task
+function deleteTask() {
+  tasks = tasks.pop()
+}
+
 
 addTaskBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     addTask()
     console.log('db: ', tasks[0]);
+})
+deleteBtn.addEventListener('click',()=>{
+    deleteTask()
 })
