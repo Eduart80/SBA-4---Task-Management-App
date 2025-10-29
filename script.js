34947693ab99890e0575f898da1f7c9e8484cd72
Eduart2021 <zeedymarina@yahoo.com>
2025-10-29 13:44:32 -0500

    add status section

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const nameInput = document.getElementById('task-name');
 const categoryInput = document.getElementById('task-category');
 const deadlineInput = document.getElementById('task-deadline');
 const addTaskBtn = document.getElementById('add-task');
+
 // DB
 let tasks = [];
+let idCount = 0
 
 // Add new 
 function addTask(){
@@ -11,16 +13,18 @@ function addTask(){
     const inName = nameInput.value
     const inCategory = categoryInput.value
     const deadlineEntry = deadlineInput.value
+    const inDeadLine = deadlineInput.value
 
     let taskAdd = {
+        id: idCount++,
         name:inName,
         category: inCategory,
-        status:deadlineEntry
+        deadline: inDeadLine,
+        status: deadlineEntry
     }
     tasks.push(taskAdd)
-    
-    
 }
+
 addTaskBtn.addEventListener('click', ()=>{
     addTask()
 })
