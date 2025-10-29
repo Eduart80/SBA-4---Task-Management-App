5237bd4f1f245ff0d9f70059430b04c43bd2fb46
Eduart2021 <zeedymarina@yahoo.com>
2025-10-29 12:52:38 -0500

    adding getby id

---
+++ script.js
@@ -0,0 +1,3 @@
+const nameInput = document.getElementById('task-name');
+const categoryInput = document.getElementById('task-category');
+const deadlineInput = document.getElementById('task-deadline');
