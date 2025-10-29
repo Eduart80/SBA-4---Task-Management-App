213ec553667313f6161bd9eb96e211e997a7de30
Eduart2021 <zeedymarina@yahoo.com>
2025-10-29 13:10:17 -0500

    adding db and add function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const nameInput = document.getElementById('task-name');
 const categoryInput = document.getElementById('task-category');
 const deadlineInput = document.getElementById('task-deadline');
+// DB
+let tasks = [];
+
+// Add new 
+function addTask(){
+    const inName = nameInput.value
+    const inCategory = categoryInput.value
+    const deadlineEntry = deadlineInput.value
+
+    let taskAdd = {
+        name:inName,
+        category: inCategory,
+        status:deadlineEntry
+    }
+}
